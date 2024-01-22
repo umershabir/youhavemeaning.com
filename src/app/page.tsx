222d45a03dd52a2102fa26f71ef2258a8ab5ac11
Umer Shabir <muhammadumershabir@gmail.com>
2024-01-22 08:43:11 +0500

    building home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "@/layouts/Home/Hero";
+import Products from "@/layouts/Home/Products";
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <Products />
     </main>
   );
 }
---
+++ src/layouts/Home/Products.tsx
@@ -0,0 +1,8 @@
+import style from "@/styles/Home.module.scss";
+export default function Products() {
+  return (
+    <section className="container">
+      <p className={style.philosophyText}>Our Products</p>
+    </section>
+  );
+}
