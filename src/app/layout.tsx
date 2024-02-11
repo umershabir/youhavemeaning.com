4f79310c9dddb0df4d117c14cd2e8c593e099383
Umer Shabir <muhammadumershabir@gmail.com>
2024-02-11 16:48:17 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import "@/styles/index.scss";
 import type { Metadata } from "next";
-// import { Inter, Noto_Serif } from "next/font/google";
 import Header from "@/layouts/Header";
 import localFont from "next/font/local";
 const TTRegular = localFont({ src: "../lib/TTNorms-Regular.otf" });
-// const TTMedium = localFont({ src: "../lib/TTNorms-Medium.otf" });
-// const TTThin = localFont({ src: "../lib/TTNorms-Thin.otf" });
-// import Header from "@/layouts/Header";
 import Footer from "@/layouts/Footer";
 
 export const metadata: Metadata = {
   title: "YHM | Best Planners & journals",
   description:
     "You have meaning: We help top 3% of population helping them to achieve their maximum potiential with best productivity tools",
+  metadataBase: new URL("https://youhavemeaning.com"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+    },
+  },
+  openGraph: {
+    images: "../assets/logo.png",
+  },
 };
 
 export default function RootLayout({
